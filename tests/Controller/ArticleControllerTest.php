238eb4d503ddb607fda31c21b555bb9f5f45848f
Emile <emile00013@gmail.com>
2024-08-23 17:31:25 +0200

    Test article delete

--- tests/Controller/ArticleControllerTest.php
+++ tests/Controller/ArticleControllerTest.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\TestCase;
 use App\Repository\UserRepository;
 use App\Repository\ArticleRepository;
 use PHPUnit\Framework\Attibutes\Test;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class ArticleControllerTest extends WebTestCase
@@ -38,7 +39,7 @@ final class ArticleControllerTest extends WebTestCase
        
         $articleApi = json_decode($client->getResponse()->getContent(), true);
 
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
         $this->assertSame($articleRepo->getId(), $articleApi['id']);
 
     }
@@ -52,10 +53,9 @@ final class ArticleControllerTest extends WebTestCase
     {
 
         $client = static::createClient();
-        $client->request('GET', '/api/articles/unpublished/');
-       
-        // $articlesApi = json_decode($client->getResponse()->getContent(), true);
-        $this->assertSame(403, $client->getResponse()->getStatusCode());
+        $client->request('GET', '/api/unpublished/articles/');
+      
+        $this->assertSame(Response::HTTP_FORBIDDEN, $client->getResponse()->getStatusCode());
 
     }
 
@@ -74,7 +74,7 @@ final class ArticleControllerTest extends WebTestCase
         $client->request('GET', '/api/unpublished/articles/');
         
         // $articlesApi = json_decode($client->getResponse()->getContent(), true);
-        $this->assertSame(403, $client->getResponse()->getStatusCode());
+        $this->assertSame(Response::HTTP_FORBIDDEN, $client->getResponse()->getStatusCode());
 
     }
 
@@ -95,11 +95,93 @@ final class ArticleControllerTest extends WebTestCase
 
         $articlesApi = json_decode($client->getResponse()->getContent(), true);
 
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
         foreach ($articlesApi as $article) {
 
             $this->assertSame(false, $articlesApi[0]['isPublished']);
 
         }
     }
+
+    /**
+     * @group unAuth
+     * @group delete
+     * @group 403
+     */
+    public function testUnauthDeleteArticles(): void
+    {
+
+        $client = static::createClient();
+        $articleRepository = static::getContainer()->get(ArticleRepository::class);
+        $count = $articleRepository->findCount();
+        $randomOffset = random_int(0, $count - 1);
+        $articles = $articleRepository->findAll();
+        $article = $articles[$randomOffset];
+
+        $id = $article->getId();
+
+        $client->request('DELETE', '/api/articles/'.$id);
+
+        $undeltedArticle = $articleRepository->findOneById($id);
+
+        $this->assertSame($article, $undeltedArticle);
+        $this->assertSame(Response::HTTP_FORBIDDEN, $client->getResponse()->getStatusCode());
+        
+    }
+
+    /**
+     * @group authUser
+     * @group delete
+     * @group 403
+     */
+    public function testAuthUserDeleteArticles(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $articleRepository = static::getContainer()->get(ArticleRepository::class);
+        $count = $articleRepository->findCount();
+        $randomOffset = random_int(0, $count - 1);
+        $articles = $articleRepository->findAll();
+        $article = $articles[$randomOffset];
+
+        $id = $article->getId();
+        $user = $userRepository->findOneByEmail('user@emile.blog');
+        $client->loginUser($user);
+
+        $client->request('DELETE', '/api/articles/'.$id);
+
+        $undeltedArticle = $articleRepository->findOneById($id);
+
+        $this->assertSame($article, $undeltedArticle);
+        $this->assertSame(Response::HTTP_FORBIDDEN, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @group authAdmin
+     * @group delete
+     * @group 204
+     */
+    public function testAuthAdminDeleteArticles(): void
+    {
+
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $articleRepository = static::getContainer()->get(ArticleRepository::class);
+        $admin = $userRepository->findOneByEmail('admin@emile.blog');
+        $count = $articleRepository->findCount();
+        $randomOffset = random_int(0, $count - 1);
+        $articles = $articleRepository->findAll();
+        $article = $articles[$randomOffset];
+
+        $id = $article->getId();
+        $client->loginUser($admin);
+
+        $client->request('DELETE', '/api/articles/'.$id);
+
+        $deltedArticle = $articleRepository->findOneById($id);
+        $this->assertSame(null, $deltedArticle);
+        $this->assertSame(Response::HTTP_NO_CONTENT, $client->getResponse()->getStatusCode());
+    }
+
+
 }
